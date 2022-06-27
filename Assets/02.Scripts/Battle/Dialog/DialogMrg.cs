09705b01f8d94d056e846068a5efd704fa25f996
LLL <77712364+elsam0104@users.noreply.github.com>
2022-06-27 17:13:52 +0900

    06/27 대사 매니저 소폭 수정

--- Assets/02.Scripts/Battle/Dialog/DialogMrg.cs
+++ Assets/02.Scripts/Battle/Dialog/DialogMrg.cs
@@ -7,10 +7,14 @@ public class DialogMrg : MonoBehaviour
 {
     [SerializeField]
     private Text dialogText;
+    [SerializeField]
+    private GameObject ojb;
     public void Start()
     {
         SetDialog("이런... 여긴 어디지");
-        NextDialog("일단 움직이자");
+    }
+    private void Update()
+    {
         EndDialog();
     }
     public void NextDialog(string s)
@@ -24,12 +28,16 @@ public class DialogMrg : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            ojb.SetActive(false);
             dialogText.gameObject.SetActive(false);
         }
     }
     public void SetDialog(string s)
     {
+        dialogText.gameObject.SetActive(true);
+        ojb.SetActive(true);
         dialogText.text = "";
         dialogText.DOText("- " + s, s.Length * 0.3f);
+
     }
 }
